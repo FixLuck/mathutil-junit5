312b726015bc0ed5b7cfd4184973c32fead8be2c
FixLuck <caube.vodoi5@gmail.com>
2023-07-26 09:03:15 +0700

    1st upload with maven project

---
+++ src/main/java/com/fixluck/mathutil/main/Main.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.fixluck.mathutil.main;
+
+import com.fixluck.mathutil.core.MathUtil;
+
+/**
+ *
+ * @author caube
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+        System.out.println("5! = " + MathUtil.getFactorial(5));
+    }
+}
